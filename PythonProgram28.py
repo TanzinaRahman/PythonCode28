03b01a1e68aba9053c613cd9810d92990f47568d
Tanzina Rahman Mona <77913975+TanzinaRahman@users.noreply.github.com>
2021-07-29 11:21:30 +0600

    Create PythonProgram28.py

---
+++ PythonProgram28.py
@@ -0,0 +1,28 @@
+subjects = ['av', 'ac', 'ji', 'et']
+print(len(subjects))
+subjects.append('toc')
+print(subjects)
+subjects.insert(3,'hu')
+print(subjects)
+subjects.remove('et')
+print(subjects)
+subjects.sort()
+print(subjects)
+subjects.pop()
+print(subjects)
+subjects.pop()
+print(subjects)
+subjects.clear()
+print(subjects)
+subjects2 = subjects.copy()
+print(subjects2)
+subjects3 = ['meio', 'rrt', 'kuio', 'rrt']
+print(subjects3)
+pos = subjects3.index('rrt')
+print(pos)
+pos = subjects3.count('rrt')
+print(pos)
+ 
+
+
+
